76dd2c4914801f42e4359bb49c0c47b6a9305f3e
arvind9140 <ar789maurya@gmail.com>
2022-11-20 12:10:08 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import { app} from "./app.js"
 import dotenv from "dotenv"
 import { connectDatabase } from "./config/database.js";
 import cloudinary from "cloudinary";
+import express from 'express'
+import path from 'path'
 
 // require('dotenv').config({path:"./config/.env"})
 dotenv.config({path:"./backend/config/.env"});
@@ -13,6 +15,14 @@ cloudinary.v2.config({
   api_key: '214435175726284', 
   api_secret: 'PDhOv_RVTqE_G4txGp6W_ubLwzw' 
 });
+
+// server production asserts
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join("frontend/build")))
+  app.get("*", (req,res) => {res.sendFile(path.resolve(_dirname,'frontend', 'build', 'index.html'))
+});
+}
 
 app.listen(process.env.PORT, ()=>{
   console.log(`Server is running on port: ${process.env.PORT}`)
